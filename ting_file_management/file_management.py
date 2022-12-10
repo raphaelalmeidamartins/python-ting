5feba590054f7bcd1ae537f7de8117628a7d27e2
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-12-10 14:55:26 -0300

    :sparkles: feat: txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            print("Formato inválido", file=sys.stderr)
+        else:
+            with open(path_file, encoding="utf-8", mode="r") as file:
+                return file.read().split("\n")
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
