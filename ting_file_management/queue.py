ff24d75959eb9e47a1a7c8d0b195ac022f2cb8e6
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-12-10 14:47:54 -0300

    :recycle: refactor: change data private propertiy name to use dunder

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,19 +1,19 @@
 class Queue:
     def __init__(self):
-        self._data = []
+        self.__data = []
 
     def __len__(self):
-        return len(self._data)
+        return len(self.__data)
 
     def enqueue(self, value):
-        self._data.append(value)
+        self.__data.append(value)
 
     def dequeue(self):
         if len(self):
-            return self._data.pop(0)
+            return self.__data.pop(0)
         return None
 
     def search(self, index):
         if 0 <= index < len(self):
-            return self._data[index]
+            return self.__data[index]
         raise IndexError("Index out of range")
