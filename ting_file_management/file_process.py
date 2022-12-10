62d7401009ef38c30c50719583c9a8473071bcf0
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-12-10 16:06:42 -0300

    :sparkles: feat: implement the process function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file, queue: Queue):
+    for item in queue:
+        if item["nome_do_arquivo"] == path_file:
+            return None
+
+    file_lines = txt_importer(path_file)
+
+    file_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_lines),
+        "linhas_do_arquivo": file_lines,
+    }
+
+    queue.enqueue(file_dict)
+    print(file_dict)
 
 
 def remove(instance):
