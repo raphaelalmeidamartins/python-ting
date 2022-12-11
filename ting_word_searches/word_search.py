87d33d8b14972914a177648b1692ff9e426e5f86
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-12-11 00:46:33 -0300

    :sparkles: feat: implement word search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,31 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def exists_word(word, queue: Queue, include_content: bool = False):
+    result = []
+
+    for file_index, file in enumerate(queue, start=1):
+        found_lines = [
+            {"conteudo": line, "linha": line_index}
+            if include_content
+            else {"linha": line_index}
+
+            for line_index, line
+            in enumerate(file["linhas_do_arquivo"], start=1)
+            if word.lower() in line.lower()
+        ]
+
+        if not len(found_lines):
+            continue
+
+        result.append({
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": found_lines,
+        })
+
+    return result
+
+
+def search_by_word(word, queue):
+    return exists_word(word, queue, True)
